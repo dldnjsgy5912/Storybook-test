9fd12610d8c00d5f23c2523e3dfa5345cface683
wonhyolee <wonhyo.lee@gcar.co.kr>
2023-02-26 23:44:58 +0900

    disabled 추가

--- stories/Button.tsx
+++ stories/Button.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './button.css';
 
 interface ButtonProps {
+  disabled?: boolean;
   /**
    * Is this the principal call to action on the page?
    */
@@ -27,21 +28,11 @@ interface ButtonProps {
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
+export const Button = ({ disabled = false, primary = false, size = 'medium', backgroundColor, label, ...props }: ButtonProps) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
+    <button type="button" disabled={disabled} className={['storybook-button', `storybook-button--${size}`, mode].join(' ')} style={{ backgroundColor }} {...props}>
       {label}
     </button>
   );
